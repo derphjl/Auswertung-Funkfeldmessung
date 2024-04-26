355857ccc3f4653be5536c744f4038b3ddc84888
flip <github@phjl.de>
2024-04-26 17:08:34 +0200

    changed parameters into an array and defined a type, filling it accordingly

--- main.js
+++ main.js
@@ -14,14 +14,22 @@ const siteReference = "Gebäude X";
 * }}
 */
 
+/**
+* @typedef Parameter
+* @type {{
+  *   title: string;
+  *   value: string;
+  *   unit: string;
+  * }}
+  */
+  
+
 // The Trace represents the single Line on the screen of the Analyzer. The properties are read from the first part of the CSV.
 // The Trace will also contain relevant, calculated information such as noise floor average
 /**
 * @typedef Trace
 * @type {{
-*   parameters: {
-*     [key: string]: string;
-*   };
+*   parameters: Parameter[];
 *   records: Record[];
 * }}
 */
@@ -84,7 +92,7 @@ try {
   // now, const measurementFolders: string[] contains the folders withing /results, sorted numerically. The numerical quantity could be accessed with measurementFolders.length
   
   console.log("☝️  " + points.length + " points were found. Now going through them...");
-
+  
   //### Section 2: Start a Loop: Commit to one Folder, get its Name and save it. Iterate. ###
   for (let currentPoint of points) {
     // call the point type as const point and initialize it with the current point string
@@ -111,10 +119,10 @@ try {
         ref: currentSnapshot,
         traces: [],
       }
-
+      
       //### Section 4: A Snapshot is a CSV File that is first split into lines ###
       let linesOfFile = ((await readFile(`./results/${currentPoint}/${currentSnapshot}`, { encoding: 'utf8' })).trim()).split('\n'); //read the file and split it into lines
-
+      
       //### Section 5: To fetch the ammount and position of Traces, seach for the Keyword "Sweep" and save the correct positions ###
       //TODO: The more elegant solution would maybe be to check the split aray of elements in a line and check for a pattern of DATA - DATA - EMPTY, returning the position of the first DATA? However, this will work for now!
       const occurencesOfSweep = linesOfFile[0].split(`,`).filter((entry) => entry.match(new RegExp(/Sweep/)));
@@ -124,17 +132,17 @@ try {
         dataIndices.push(linesOfFile[0].split(`,`).indexOf(occurenceOfSweep) - 1);
       }
       //dataIndices is now an array containing the starting positions for the data of each trace.
-
+      
       //## Section 6: With dataIndex now representing a single Trace, we start extracting the Data for the trace.
       for (let dataIndex of dataIndices) {
-      //With dataIndex now being the starting point of the relevant Data in this loop/Trace, begin a Trace.
-      //in scope, dataInxdex describes the trace start position, meaning for every line, the relevant points are, in the case of the head, dataIndex as the key, dataindex + 1 as the value and  dataIndex + 2 as the unit which shall be reduced into the value (it's probably never going to be used anyway)
-      /**
-      * @type {Trace}
-      */
-      const trace = {
-        parameters : {},
-        records : [],
+        //With dataIndex now being the starting point of the relevant Data in this loop/Trace, begin a Trace.
+        //in scope, dataInxdex describes the trace start position, meaning for every line, the relevant points are, in the case of the head, dataIndex as the key, dataindex + 1 as the value and  dataIndex + 2 as the unit which shall be reduced into the value (it's probably never going to be used anyway)
+        /**
+        * @type {Trace}
+        */
+        const trace = {
+          parameters : [],
+          records : [],
         }
         //splitIndex is the line before the line containg the keyword "Frequency [Hz]" and marks the vertical split between the parameters-part and the records-part of the trace
         let splitIndex = linesOfFile.findIndex((singleLine) => singleLine.match(new RegExp(/.*Frequency \[Hz].*/))) - 1; //first we find out where the key "Frequency [Hz] is. This splits Head and Data.
@@ -142,10 +150,15 @@ try {
         //only working within the parameters-portion of the trace: the key is in the first column, the value in the second and the unit in the third. Wer are producing key-value-pairs, so the unit gets smashed onto the value if applicable.
         for (let lineOfFile of linesOfFile.slice(0,splitIndex)) {
           let elementsOfLine = lineOfFile.split(`,`);
-          let key = elementsOfLine[dataIndex];
-          //TODO: work out a better solution here. smashing the value and its unit is not elegant.
-          let value = ((elementsOfLine[dataIndex + 1] + " " +  elementsOfLine[dataIndex + 2]).trim());
-          trace.parameters[key] = value;
+          /**
+          * @type {Parameter}
+          */
+          let parameter = {
+            title : elementsOfLine[dataIndex],
+            value : elementsOfLine[dataIndex + 1],
+            unit : elementsOfLine[dataIndex + 2],
+          };
+          trace.parameters.push(parameter);
         }
         
         for (let lineOfFile of linesOfFile.slice(splitIndex + 2)) {
@@ -171,15 +184,15 @@ try {
     console.log("✅ Data for Point " + currentPoint + " with " + snapshots.length + " snapshots imported!")
   } // * end of point *
   console.log("🎉 All points transfered into defined data structure!");   
-
-  console.log(site.points[3].snapshots[6].traces[1].parameters)
-
+  
+  console.log(site.points[3].snapshots[6].traces[1])
+  
   //TODO: Work out analysis of the data
-
-
+  
+  
   //TODO: Export the Analysis in a sensical way
-
-
+  
+  
 } catch (error) {
   console.error('there was an error:', error.message);
 }
